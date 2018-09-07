eef21fb843d58d68405ce8f4d86192f99bea7f4d
SilverNeko21 <larong2122@gmail.com>
2018-09-07 13:25:49 -1000

    oof

---
+++ let-const.js
@@ -0,0 +1,26 @@
+// const
+const name = 'laron';
+console.log('name:', name);
+// can't change what is inside the variable
+
+
+// let
+let dog = 'toetoe';
+
+dog = 'jack';
+console.log('dog:', dog);
+// lets you change what is in the variable.
+
+
+// difference between var and let
+ var greeting = () => {
+     var greetingSentence = 'Hello World!';
+     console.log('greetingSentence', greetingSentence);
+ }
+ console.log('greetingSentence', greetingSentence);
+
+
+if(1 === 1) {
+    let greet = 'Hello';
+}
+console.log('greet', greet);
